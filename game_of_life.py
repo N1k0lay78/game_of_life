36ef135134cd146697742edca28242c7da11da67
Rjkzavr <kolya.toropof@gmail.com>
2025-01-09 18:46:36 +0300

    tmp

--- game_of_life.py
+++ game_of_life.py
@@ -13,13 +13,24 @@ class GameOfLife:
             [' ', ' ',' ',' ',' ', ' ', ' ',' ',' ',' ',],
         ]
     
+    def get_cell(self, x, y):
+        return self.world[y][x]
+
+    def set_cell(self, x, y, value):
+        self.world[y][x] = value
+
     def get_count_life_around(self, x, y):
         # будет возвращать количество живых клеток вокруг
         pass
     
     def update_cell(self, x, y):
-        pass
+        cell = self.get_cell(x, y)
+        if cell == ' ':
+            if self.get_count_life_around(x, y) == 3:
+                value = 'jizn'
+                self.set_cell(x, y, value)
 
     def update_world(self):
-        # здесь обновляем все клетки по очереди
-        pass
+        for y in range(len(self.world)):
+            for x in range(len(self.world[y])):
+                self.update_cell(x, y)
