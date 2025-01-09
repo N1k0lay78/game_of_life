3df2e6d349321df2d1ab3286d8fd98a9dd7e302d
bsmtx <kondratevnikita2000@mail.ru>
2025-01-09 20:10:28 +0400

    Update game_of_life.py

--- game_of_life.py
+++ game_of_life.py
@@ -20,16 +20,28 @@ class GameOfLife:
         self.world[y][x] = value
 
     def get_count_life_around(self, x, y):
+        count = 0
         # будет возвращать количество живых клеток вокруг
-        pass
-    
+        for y1 in range(-1, 2):
+            for x1 in range(-1, 2):
+                if x1 != 0 and y1 != 0:
+                    x += x1 
+                    y += y1
+                    x = x % len(self.world[0])
+                    y = y % len(self.world)
+                    if self.get_cell(x, y) == 'jizn':
+                        count += 1
+        return count                                
     def update_cell(self, x, y):
         cell = self.get_cell(x, y)
         if cell == ' ':
             if self.get_count_life_around(x, y) == 3:
                 value = 'jizn'
                 self.set_cell(x, y, value)
-
+        else:
+            if not (2 <= self.get_count_life_around(x, y) <= 3):
+                value = ' '
+                self.set_cell(x, y, value)
     def update_world(self):
         for y in range(len(self.world)):
             for x in range(len(self.world[y])):
